4d79f606082636b93695ac568caa443c157eedff
bryanongwx <bryanongwenxi@gmail.com>
2022-05-07 16:45:10 +0800

    Trial of finding names

--- Scraper.py
+++ Scraper.py
@@ -2,5 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://www.sgdi.gov.sg/ministries/mti/statutory-boards/jtc/departments/eog').text
-print(html_text)
+soup = BeautifulSoup(html_text, 'lxml')
+names = soup.find_all('span', class_ = 'left')
 
+print(names)
